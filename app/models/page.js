9ab5fdc3a7bd1f77253483ce2e869dfc46fb56fa
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2017-05-01 19:55:40 -0400

    Commented Admin User Schema

--- app/models/page.js
+++ app/models/page.js
@@ -1,12 +1,12 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var pageSchema = new Schema({
-  title: {type: String, require:false},
-  url: {type: String, index:{unique:true}}, //set to unique to avoid duplicate URL aliases
-  content: {type: String},
-  menuIndex: {type:Number},
-  data: Date
-}):
-
-module.export = mongoose.model('Page', pageSchema);
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+//
+// var pageSchema = new Schema({
+//   title: {type: String, require:false},
+//   url: {type: String, index:{unique:true}}, //set to unique to avoid duplicate URL aliases
+//   content: {type: String},
+//   menuIndex: {type:Number},
+//   data: Date
+// }):
+//
+// module.export = mongoose.model('Page', pageSchema);
